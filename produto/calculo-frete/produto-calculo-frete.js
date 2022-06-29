b85a2f4acd9ecb2defeb867642a8096769467efe
Fabio Santos <fabiosantosrj@gmail.com>
2022-06-29 08:09:45 -0300

    Update produto-calculo-frete.js
    Ajustes na descrição detalhada e para esconder entregas com informações iguais.

--- produto/calculo-frete/produto-calculo-frete.js
+++ produto/calculo-frete/produto-calculo-frete.js
@@ -1,341 +1,3 @@
-$(function () {
-  if ($('body.categoria').length) {
-    $('.filtrarpor').on('click', function () {
-      console.log(".filtrarpor click");
-      $('.filtrarpor.submenu-departamento').toggleClass('-active');
-    });
-
-    $("#filtrarpor [data-filtro]").on("click", function () {
-      let url = window.location.href;
-      url += (window.location.href.indexOf("?") == -1) ? "?" : (window.location.href.length - 1 == window.location.href.indexOf("?")) ? "" : "&";
-      if (url.indexOf("?O=") > -1 || url.indexOf("&O=") > -1) {
-        let urlVet = url.split("?");
-        let posRemove = -1;
-        if (urlVet.length == 2) {
-          urlVet = urlVet[1].split("&");
-          for (i = 0; i < urlVet.length; urlVet++) {
-            let item = urlVet[i];
-            item = item.split("=");
-            if (item[0] == "O") {
-              posRemove = i;
-              break;
-            }
-          }
-          if (posRemove > -1) {
-            urlVet.splice(posRemove, 1);
-          }
-          url = url.substr(0, url.indexOf("?") + 1) + urlVet.join("&");
-        }
-      }
-      url += "O=" + $(this).attr("data-filtro");
-      window.location.href = url;
-    });
-  }
-});
-
-$(function () {
-  $('[id^="helperComplement_"]').remove();
-  var e = $('.button-buy-add-cart'),
-    t = $('.shelf-btn-menos'),
-    o = $('.shelf-btn-mais');
-  function a() {
-    vtexjs.checkout.getOrderForm({ cache: !1 }).done(function (e) {
-      let t = document.querySelector('.mini-cart-itens'),
-        o = new Object();
-      if (e.items.length > 0) {
-        $('.mini-cart-qty-admake').html(e.items.length), (t.innerHTML = '');
-        let a,
-          s = 0,
-          i = e.value / 100;
-        e.items.forEach(function (e, i) {
-          let n = (e.price / 100).toString().replace('.', ',');
-          (a = `\n                  <div class="mini-cart-item">\n                      <span class="imagem">\n                          <a class="sku-imagem" href="${e.detailUrl}">\n                              <img src="${e.imageUrl}"/>\n                          </a>\n                      </span>\n                      <span class="detalhes">\n                          <p class="nome-produto">\n                              <a href="${e.detailUrl}">${e.name}</a>\n                          </p>\n                          <span class="qtd-valor">\n                              <span class="qtd">${e.quantity}x</span>\n                              <span class="preco">${n}</span>\n                          </span>\n                      </span>\n                  </div>\n                `),
-            (t.innerHTML += a),
-            (a = ''),
-            (o[i] = { price: parseInt(e.price), qtd: e.quantity }),
-            ++s;
-        }),
-          $('#mini-cart-admake-total').html(
-            i.toLocaleString('pt-BR', {
-              minimumFractionDigits: 2,
-              maximumFractionDigits: 2,
-            })
-          );
-      }
-    });
-  }
-  e.length &&
-    e.on('click', function () {
-      var e = $(this).siblings().attr('item-id'),
-        t = $(this).siblings().find('.shelf-qtd').val();
-      vtexjs.checkout.getOrderForm({ cache: !1 }).done(function () {
-        var o = { id: e, quantity: t, seller: '1' };
-        vtexjs.checkout.addToCart([o]).done(function (e) {
-          $('.cart-loading').show(),
-            $('.mini-cart-message').addClass('show'),
-            setTimeout(function () {
-              $('.cart-loading').hide(), $('.sucess').addClass('active-show');
-            }, 800),
-            setTimeout(function () {
-              $('.mini-cart-message').removeClass('show'),
-                $('.sucess').removeClass('active-show');
-            }, 2500),
-            a();
-        });
-      });
-    }),
-    a(),
-    (function () {
-      const e =
-        '/api/catalog_system/pub/products/search' + window.location.pathname;
-      $.ajax(e, 'GET', 'jsonp').then((e) => {
-        if (e[0].Validade) {
-          const t = e[0].Validade;
-          $('.validate-box').append('<p>validade do produto ' + t + '</p>');
-        }
-      });
-    })();
-}),
-  $(function () {
-    $('.menu-burger').on('click', function () {
-      $('.nav-mobile').addClass('-active');
-    }),
-      $('.submenu-mobile').hide(),
-      $('.link-main-menu-mobile').on('click', function () {
-        $(this).next('.submenu-mobile').toggle(), $(this).toggleClass('-open');
-      }),
-      $('.js-toggle-menu').on('click', function () {
-        $('.nav-mobile').removeClass('-active');
-      }),
-      $(window).width() < 1199 &&
-      ($('.bar-top').addClass('-scroll'), $('.header').addClass('-scroll')),
-      $(window).scroll(function () {
-        $(window).scrollTop() > 150
-          ? ($('.bar-top').addClass('-scroll'),
-            $('.header').addClass('-scroll'))
-          : ($('.bar-top').removeClass('-scroll'),
-            $('.header').removeClass('-scroll'));
-      });
-  }),
-  $(function () {
-    $('.home').length &&
-      ($('.banner-desk, .banner-mob').slick({
-        dots: !0,
-        arrows: !0,
-        infinite: !0,
-        autoplay: !0,
-        autoplaySpeed: 3e3,
-        slidesToShow: 1,
-        slidesToScroll: 1,
-      }),
-        $('.prateleira ul').slick({
-          dots: !1,
-          arrows: !0,
-          infinite: !0,
-          autoplay: !1,
-          autoplaySpeed: 3e3,
-          slidesToShow: 4,
-          slidesToScroll: 1,
-          responsive: [
-            {
-              breakpoint: 992,
-              settings: {
-                arrows: !0,
-                slidesToShow: 4,
-                slidesToScroll: 1,
-                infinite: !0,
-                autoplay: !1,
-                autoplaySpeed: 3e3,
-                dots: !1,
-              },
-            },
-            {
-              breakpoint: 768,
-              settings: {
-                arrows: !0,
-                slidesToShow: 2,
-                autoplay: !0,
-                infinite: !0,
-                autoplaySpeed: 3e3,
-                slidesToScroll: 1,
-              },
-            },
-            {
-              breakpoint: 480,
-              settings: {
-                arrows: !0,
-                slidesToShow: 2,
-                infinite: !0,
-                autoplay: !0,
-                autoplaySpeed: 3e3,
-                slidesToScroll: 1,
-              },
-            },
-          ],
-        }),
-        $('.partners .parceiros').slick({
-          dots: !1,
-          arrows: !0,
-          infinite: !1,
-          speed: 300,
-          autoplay: !0,
-          slidesToShow: 5,
-          slidesToScroll: 1,
-          autoplay: !0,
-          autoplaySpeed: 3e3,
-          responsive: [
-            {
-              breakpoint: 1024,
-              settings: {
-                arrows: !0,
-                slidesToShow: 5,
-                slidesToScroll: 5,
-                infinite: !0,
-                autoplay: !0,
-                autoplaySpeed: 3e3,
-                dots: !1,
-              },
-            },
-            {
-              breakpoint: 600,
-              settings: {
-                arrows: !0,
-                slidesToShow: 1,
-                infinite: !0,
-                autoplay: !0,
-                autoplaySpeed: 3e3,
-                slidesToScroll: 1,
-              },
-            },
-            {
-              breakpoint: 480,
-              settings: {
-                arrows: !0,
-                slidesToShow: 2,
-                infinite: !0,
-                autoplay: !0,
-                autoplaySpeed: 3e3,
-                slidesToScroll: 1,
-              },
-            },
-          ],
-        }),
-        $(window).width() < 500 &&
-        ($('.badges .wrapper').slick({
-          dots: !1,
-          arrows: !0,
-          infinite: !0,
-          speed: 300,
-          slidesToShow: 1,
-          autoplay: !0,
-          autoplaySpeed: 3e3,
-          slidesToScroll: 1,
-        }),
-          $('.destaques-mobile').slick({
-            dots: !1,
-            arrows: !0,
-            infinite: !0,
-            speed: 300,
-            slidesToShow: 1,
-            autoplay: !0,
-            autoplaySpeed: 3e3,
-            slidesToScroll: 1,
-          })));
-  }),
-  $(function () {
-    if ($('.produto').length) {
-      $('.prateleira ul').slick({
-        dots: !1,
-        arrows: !0,
-        infinite: !0,
-        autoplay: !1,
-        autoplaySpeed: 3e3,
-        slidesToShow: 4,
-        slidesToScroll: 1,
-        responsive: [
-          {
-            breakpoint: 992,
-            settings: {
-              arrows: !0,
-              slidesToShow: 4,
-              slidesToScroll: 1,
-              infinite: !0,
-              autoplay: !1,
-              autoplaySpeed: 3e3,
-              dots: !1,
-            },
-          },
-          {
-            breakpoint: 768,
-            settings: {
-              arrows: !0,
-              slidesToShow: 2,
-              autoplay: !1,
-              infinite: !0,
-              autoplaySpeed: 3e3,
-              slidesToScroll: 1,
-            },
-          },
-          {
-            breakpoint: 480,
-            settings: {
-              arrows: !0,
-              slidesToShow: 2,
-              infinite: !0,
-              autoplay: !1,
-              autoplaySpeed: 3e3,
-              slidesToScroll: 1,
-            },
-          },
-        ],
-      });
-      const t = $('#image-main'),
-        o = $('.image-zoom');
-      if (t.length) {
-        const e = t.attr('src').replace('-292-292', '-400-400');
-        t.attr('src', e),
-          o.on('click', function (e) {
-            e.preventDefault();
-          });
-      }
-      var e = $('.buy-button-box');
-      e.length &&
-        (e.prepend(
-          '<div class="pull-left box-qtd">\t<div class="bts pull-left">\t\t<button class="btn btn-menos">-</button>\t  <input type="text" class="qtd pull-left" value="1" />\t\t<button class="btn btn-mais">+</button> \t</div></div>'
-        ),
-          $(document).on(
-            'keypress',
-            '.buy-button-box .box-qtd .qtd',
-            function (e) {
-              var t = window.event ? event.keyCode : e.which;
-              return (t > 47 && t < 58) || 8 == t || 0 == t;
-            }
-          ),
-          $(document).on('keyup', '.buy-button-box .box-qtd .qtd', function (e) {
-            $('.buy-button-box .box-qtd .qtd').val($(this).val());
-          }),
-          $(document).on('blur', '.buy-button-box .box-qtd .qtd', function (e) {
-            var t = $(this);
-            '' === t.val() || parseInt(t.val()) < 1
-              ? $('.buy-button-box .box-qtd .qtd').val(1)
-              : $('.buy-button-box .box-qtd .qtd').val(t.val());
-          }),
-          $(document).on('click', '.buy-button-box .box-qtd .btn', function () {
-            var e = $(this),
-              t = $('.buy-button-box .box-qtd .qtd'),
-              o = parseInt(t.val());
-            e.hasClass('btn-mais')
-              ? t.val(o + 1)
-              : e.hasClass('btn-menos') && o > 1 && t.val(o - 1);
-          })),
-        setTimeout(() => {
-          $('.freight-zip-box').attr('placeholder', '00000-000');
-        }, 500);
-
-      calculoFrete.init({ btnOk: ".freight-btn", tipoLista: "resumido", el: ".freight-values" });
-    }
-  });
-
 var calculoFrete = {
   data: {
     btnOk: '.freight-btn',
@@ -378,15 +40,18 @@ var calculoFrete = {
       let obj = calculoFrete;
       let objData = obj.data;
       let freteLista = [];
-      let freteItemPadrao = { preco: null, descricao: null, tempo: null, delivery: true }
+      let freteItemPadrao = { id: null, preco: null, descricao: null, tempo: null, delivery: true }
       let freteItemsMaisRapido = { ...freteItemPadrao };
       let freteItemsMaisBarato = { ...freteItemPadrao };
+      let freteItems = [];
 
-      let freteItems = $(dataResult).find("tbody tr").map((posicao, item) => {
-        let $item = $(item);
+      for (let posicao = 0; posicao < $(dataResult).find("tbody tr").length; posicao++) {
+        let $item = $($(dataResult).find("tbody tr")[posicao]);
         let descricao = $($item.find("td")[1]).text();
         let tempo = descricao;
         let freteItem = { ...freteItemPadrao };
+
+        freteItem.id = posicao;
 
         freteItem.preco = $($item.find("td")[0]).text().replace("R$", "").replace(",", ".");
         if (freteItem.preco.indexOf("Frete Grátis") > -1) {
@@ -409,8 +74,10 @@ var calculoFrete = {
 
         freteItem.delivery = descricao.indexOf("(") == -1 && descricao.indexOf(")") == -1;
 
-        if (!freteItem.delivery) {
-          descricao = descricao.substr(0, descricao.indexOf("(") - 1);
+        if (freteItem.delivery) {
+          descricao = descricao.substr(0, descricao.indexOf(",") - 1).trim();
+        } else {
+          descricao = descricao.substr(0, descricao.indexOf("(") - 1).trim();
         }
         freteItem.descricao = descricao;
 
@@ -423,19 +90,19 @@ var calculoFrete = {
           }
         }
 
-        return freteItem;
-      });
+        freteItems.push(freteItem);
+      }
+
+      //inclui todas as retiradas em loja
+      let freteListaRetirada = freteItems.filter(x => x.delivery == false);
+      for (let i = 0; i < freteListaRetirada.length; i++) {
+        let item = freteListaRetirada[i];
+        if (freteLista.find(x => x.descricao == item.descricao && x.preco == item.preco && x.tempo == item.tempo) == undefined) {
+          freteLista.push(item);
+        }
+      }
 
       if (objData.tipoLista == "resumido") {
-        //inclui todas as retiradas em loja
-        freteLista = freteItems.filter(x => x.delivery == false);
-        for (let i = 0; i < freteItems.length; i++) {
-          let item = freteItems[i];
-          if (!item.delivery) {
-            freteLista.push(item);
-          }
-        }
-
         if (freteItemsMaisBarato.preco != null) {
           freteItemsMaisBarato.descricao = "Frete mais barato";
           freteLista.push(freteItemsMaisBarato);
@@ -446,7 +113,13 @@ var calculoFrete = {
           freteLista.push(freteItemsMaisRapido);
         }
       } else {
-        freteLista = freteItems;
+        let freteListaDelivery = freteItems.filter(x => x.delivery == true);
+        for (let i = 0; i < freteListaDelivery.length; i++) {
+          let item = freteListaDelivery[i];
+          if (freteLista.find(x => x.descricao == item.descricao && x.preco == item.preco && x.tempo == item.tempo) == undefined) {
+            freteLista.push(item);
+          }
+        }
       }
 
       $(objData.el).addClass((freteLista.length > 0) ? "resultado-ok" : "resultado-vazio");
